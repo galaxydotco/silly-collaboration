fb0db6948df906520f113ad137d7570aac90e5c5
galaxydotco <spamm677o@gmail.com>
2026-04-18 11:10:24 -0500

    recreated filepath

--- assets/data/data.js
+++
@@ -1,5 +0,0 @@
-const jsObjectData = [
-    { action: "Open New Tab", windows: "Windows + Print Screen"},
-    { action: "Copy", windows: "Ctrl + C"},
-    { action: "Paste", windows: "Ctrl + V"},
-  ];
